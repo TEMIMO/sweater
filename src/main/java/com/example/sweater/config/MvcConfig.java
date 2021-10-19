9b3b3435ef6e1a0df4006e3cc0444c3516aedffb
TEMIMO <artemymif@gmail.com>
2021-10-19 20:03:37 +0300

    Turbolinks added

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.example.sweater.config;
 
+import com.example.sweater.util.RedirectInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -28,5 +30,10 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("file:///" + uploadPath + "/"); // file указывает на место где-то в файловой системе
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");//При обращении ресурсы по этому пути будут искаться не в файловой системе а с корня проекта
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectInterceptor());
     }
 }
